0fb3be2655c7ac9ad34e6fcf9ef2aabf5ef6df68
kamran-meer-ali <kamranmeer2045@gmail.com>
2024-11-15 19:42:41 +0500

    Add files via upload

---
+++ SaylaniAssignment/app.js
@@ -0,0 +1,6 @@
+function setNewImg(){
+    document.getElementById('img').src="image/Latest Collection of bing hd wallpapers.jpeg";
+}
+function setOutImg(){
+   document.getElementById('img').src="image/amir.jpeg";
+}
